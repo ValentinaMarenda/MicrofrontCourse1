95fe93fe82e3286626c429e7cae7952f1bb250bc
valentinaContabilium <valentina.marenda@contabilium.com>
2022-04-26 16:22:35 -0300

    add invalidation step to build

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>helloo!!!!</h1>
+        <h1>helloo!!!</h1>
         <hr/>
         <MarketingApp/>
     </div>
