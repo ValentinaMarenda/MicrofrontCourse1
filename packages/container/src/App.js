b07eeb3396ec74c9059646046975348c93f437e8
valentinaContabilium <valentina.marenda@contabilium.com>
2022-04-26 15:32:07 -0300

    change workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>hi there!!!!</h1>
+        <h1>hii!!!!</h1>
         <hr/>
         <MarketingApp/>
     </div>
