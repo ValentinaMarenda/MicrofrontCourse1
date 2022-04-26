7165518b403c19ee13a98ba90af72b4ee3ede304
valentinaContabilium <valentina.marenda@contabilium.com>
2022-04-26 11:18:18 -0300

    changed webpack.prod

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,4 +1,4 @@
-const {merge} = require('webpack-merge');
+const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
@@ -6,19 +6,19 @@ const packageJson = require('../package.json');
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-    mode: 'production',
+    mode: 'production', 
     output:{
-        filename: '[name].[contenthash].js'
-    },
+        filename: '[name].[contenthash].js',  
+    }, 
     plugins: [
         new ModuleFederationPlugin({
-            name:'container',
-            remotes:{
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+            name: 'container', 
+            remotes: {
+                marketing: `marketing@${domain}marketing/remoteEntry.js`,
             },
-            shared: packageJson.dependencies
+            shared: packageJson.dependencies,
         })
     ]
-}
+};
 
-module.exports = merge(commonConfig, prodConfig)
+module.exports = merge(commonConfig, prodConfig);
