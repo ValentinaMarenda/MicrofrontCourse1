3590d7aeef2dbdb37d114afcc7333c79e0fa1eb2
valentinaContabilium <valentina.marenda@contabilium.com>
2022-04-29 15:04:10 -0300

    added auth subapp 3

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -5,8 +5,8 @@ const packageJson = require('../package.json')
 
 const devConfig = {
     mode: 'development',
-    output:{
-        publicPath: 'http://localhost:8080/'
+    output: {
+        publicPath: 'http://localhost:8080/', 
     },
     devServer: {
         port: 8080,
@@ -19,12 +19,13 @@ const devConfig = {
             name: 'container',
             remotes:{
                 marketing: 'marketing@http://localhost:8081/remoteEntry.js', //"marketing@" have to match up with the name maketing that we worte inside marketing web dev file
-                auth: 'auth@http://localhost:8082/remoteEntry.js'
+                auth: 'auth@http://localhost:8082/remoteEntry.js',
+
             },
             shared: [packageJson.dependencies],
            // shared: ['react', 'react-dom'] // reduce the number of duplicate dependencies
         }),
-    ]
+    ],
 }
 
 module.exports = merge(commonConfig, devConfig)
